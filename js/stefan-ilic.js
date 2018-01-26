7ce543ffc1c2670d0b15730fda979f11081393f0
Stefan Ilic <stefan.ilic.vie@gmail.com>
2018-01-26 03:08:37 +0100

    Add smooth scroll js function

--- js/stefan-ilic.js
+++ js/stefan-ilic.js
@@ -2,10 +2,30 @@
 $(window).scroll(function(){
 	if ($(document).scrollTop() > 0){
 		$('nav').addClass('scrolled');
-		$('nav div ul li a').css('color', 'black');
+		$('nav div ul li a').css('color', 'white');
 	}
 	else{
 		$('nav').removeClass('scrolled');
 		$('nav div ul li a').css('color', 'white');
 	}
 });
+
+//smooth scroll function
+$(document).ready(function(){
+	$("#navigation li a").on('click', function(event) {
+	  if (this.hash !== "") {
+		event.preventDefault();
+		var hash = this.hash;
+		if (hash == "#home"){
+			$('html, body').animate({
+				scrollTop: 0
+				}, 800);
+		}
+		else {
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top-50
+				}, 800);
+		}
+	  }
+	});
+  });
